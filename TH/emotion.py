9288593d4b321ddf5c8dae4517dca0d1df4cbeb4
INGYEOMENTAL <37355339+TAEHYOKIM@users.noreply.github.com>
2018-05-01 22:29:30 +0900

    수정완료

--- TH/emotion.py
+++ TH/emotion.py
@@ -5,7 +5,13 @@
 import librosa
 import os
 import numpy as np
+import tensorflow as tf
 
+
+path = '/Users/hbk/data/python-docs-samples/speech/cloud-client/sampling_cut/' # 음성파일 저장된 폴더경로
+model_path = "/Users/hbk/data/model/"
+    
+    
 ## 녹음파일 샘플들 array 형으로 적재하는 함수
 
 def arrayStack(path): # input : 경로값 (ex. 'Users/.../')
@@ -22,87 +28,132 @@ def arrayStack(path): # input : 경로값 (ex. 'Users/.../')
         elif label == 'J': # 즐거움
             l = 2
         elif label == 'A': # 화남
-            l = 3
-        
+            l = 3   
+            
         r,sr = librosa.load(path+i) # librosa 사용
         mfcc = librosa.feature.mfcc(r,sr)
-        a = np.mean(mfcc,axis=1) # 열 평균값 구함
+        a = np.mean(mfcc,axis=1) # 열 중간값 구함
         
         x = np.vstack([x,a]) # 적재
         y = np.vstack([y,np.array(l)]) # 적재
         
-    return x, y
-
-#x,y = arrayStack('/Users/hbk/data/python-docs-samples/speech/cloud-client/sampling_cut/')
+    return x[1:], y[1:]
 
 
-x_train = x[1:128]
-y_train = y[1:128]
 
-x_test = x[128:]
-y_test = y[128:]
+## 훈련데이터, 테스트데이터 세팅하는 함수
 
-
-## 데이터프레임 넣기
-import pandas as pd
-from pandas import DataFrame,Series
-
-X = DataFrame(x[1:])
-Y = DataFrame(y[1:])
-pd.merge(X,Y,left_index = True, right_index=True)
-
-
-## softmax 
-
-import tensorflow as tf
-
-X = tf.placeholder(tf.float32, [None, 20])
-Y = tf.placeholder(tf.int32, [None, 1])  
-
-Y_one_hot = tf.one_hot(Y, 4)  # 감정 4가지
-Y_one_hot = tf.reshape(Y_one_hot, [-1, 4]) 
-
-W = tf.Variable(tf.random_normal([20, 4]), name='weight')
-b = tf.Variable(tf.random_normal([4]), name='bias')
-
-logits = tf.matmul(X, W) + b
-hypothesis = tf.nn.softmax(logits)
-
-cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits,labels=Y_one_hot))
-optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001).minimize(cost)
-
-prediction = tf.argmax(hypothesis, 1)
-correct_prediction = tf.equal(prediction, tf.argmax(Y_one_hot, 1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-
-sess = tf.Session() # session open
-sess.run(tf.global_variables_initializer())
-
-for step in range(7001):
-    sess.run(optimizer, feed_dict={X: x_train, Y: y_train})
-    if step % 100 == 0:
-        loss, acc = sess.run([cost, accuracy], feed_dict={ X: x_train, Y: y_train})
-        print("Step: {:5}\tLoss: {:.3f}\tAcc: {:.2%}".format( step, loss, acc))
+def dataSet(x,y): # x : 데이터값, y : 라벨값
+    n = len(x)
+    import random
+    train_index = random.sample(range(n),int(n*0.8))
+    test_index = [i for i in range(n) if i not in train_index]
+    
+    from sklearn.preprocessing import normalize
+    # 훈련데이터는 전체데이터 80% 랜덤 추출
+    x_train = normalize(x[train_index]) 
+    y_train = y[train_index]
+    # 테스트데이터는 전체데이터 20% 랜덤 추출
+    x_test = normalize(x[test_index])
+    y_test = y[test_index]
+    
+    return x_train, y_train, x_test, y_test
 
 
-pred = sess.run(prediction, feed_dict={X: x_test})
-pred
-acc = sess.run(accuracy, feed_dict={X: x_test, Y: y_test})
-acc 
-sum(pred == y_test.reshape(33,))/33
 
-pred = sess.run(prediction, feed_dict={X: x_test[[2]]})
-acc = sess.run(accuracy, feed_dict={X: x_test[[2]], Y: y_test[[2]]})
+## model 생성
 
-if pred[0] == 0:
-    print("감정 : %s, 정확도 : %.2f" %("슬픔",acc))
-elif pred[0] == 1:
-    print("감정 : %s, 정확도 : %.2f" %("중립",acc))
-elif pred[0] == 2:
-    print("감정 : %s, 정확도 : %.2f" %("기쁨",acc))
-elif pred[0] == 3:
-    print("감정 : %s, 정확도 : %.2f" %("화남",acc))
+def emotionModel(x_train,y_train,model_path): # model_path : model 저장할 위치 입력
+    X = tf.placeholder(tf.float32, [None, 20])
+    Y = tf.placeholder(tf.int32, [None, 1])  
+    
+    Y_one_hot = tf.one_hot(Y, 4)  # 감정 4가지
+    Y_one_hot = tf.reshape(Y_one_hot, [-1, 4]) 
+    
+    W = tf.Variable(tf.random_normal([20, 4]), name='weight1')
+    b = tf.Variable(tf.random_normal([4]), name='bias1')
 
-# 84% 까지 나옴
-sess.close() # session close
+    logits = tf.matmul(X, W) + b
+    hypothesis = tf.nn.softmax(logits)
+    
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits,labels=Y_one_hot))
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001).minimize(cost)
+    #optimizer = tf.train.AdamOptimizer(learning_rate=0.001).minimize(cost)
+    
+    prediction = tf.argmax(hypothesis, 1)
+    correct_prediction = tf.equal(prediction, tf.argmax(Y_one_hot, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    saver = tf.train.Saver() # 저장 선언
+    
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        for step in range(7001):
+            sess.run(optimizer, feed_dict={X: x_train, Y: y_train})
+            if step % 100 == 0:
+                loss, acc = sess.run([cost, accuracy], feed_dict={ X: x_train, Y: y_train})
+                print("Step: {:5}\tLoss: {:.3f}\tAcc: {:.2%}".format( step, loss, acc))
+        
+        save_path = saver.save(sess, model_path+'emotion_model') # 최종 학습된 모델 저장
+
+    print("Model saved in file: %s" % save_path)
+
+    return save_path
+
+
+
+
+## model 검증
+def modelTest(x_test,y_test,save_path):
+    X = tf.placeholder(tf.float32, [None, 20])
+    Y = tf.placeholder(tf.int32, [None, 1])  
+    
+    Y_one_hot = tf.one_hot(Y, 4)  # 감정 4가지
+    Y_one_hot = tf.reshape(Y_one_hot, [-1, 4]) 
+    
+    W = tf.Variable(tf.random_normal([20, 4]), name='weight1')
+    b = tf.Variable(tf.random_normal([4]), name='bias1')
+
+    logits = tf.matmul(X, W) + b
+    hypothesis = tf.nn.softmax(logits)
+    
+    prediction = tf.argmax(hypothesis, 1)
+    correct_prediction = tf.equal(prediction, tf.argmax(Y_one_hot, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    saver_new = tf.train.Saver() # 저장 선언
+
+    with tf.Session() as sess:
+        # 기존 저장된 모델을 불러온다
+        sess.run(tf.global_variables_initializer())
+        saver_new = tf.train.import_meta_graph(save_path+".meta")  # 모델저장경로
+        saver_new.restore(sess, save_path)  
+        tf.get_default_graph().as_graph_def()
+
+        print("예측결과")
+      
+        # 모델을 사용하여 작업    
+        pred = sess.run(prediction, feed_dict={X: x_test})  # 기존 학습모델에 의해 나온 예측값
+        acc = sess.run(accuracy, feed_dict={X: x_test, Y: y_test})  # 실제와 비교 (정확도)
+        for i in pred:
+            if i == 0:
+                print("감정 : %s" %"슬픔")
+            elif i == 1:
+                print("감정 : %s" %"중립")
+            elif i == 2:
+                print("감정 : %s" %"기쁨")
+            elif i == 3:
+                print("감정 : %s" %"화남")
+        print("정확도 : %.2f" %acc)
+      
+
+ 
+
+if __name__ == '__main__':
+
+    x,y = arrayStack(path)
+    x_train,y_train,x_test,y_test = dataSet(x,y)
+    
+    tf.reset_default_graph() 
+    save_path = emotionModel(x_train,y_train,model_path)
+    tf.reset_default_graph() 
+    modelTest(x_test,y_test,save_path)
 
