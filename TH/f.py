cae2b7fa9ca68a6b9e7d33942bbbd65a120ae15c
INGYEOMENTAL <37355339+TAEHYOKIM@users.noreply.github.com>
2018-05-03 04:00:01 +0900

    최종

--- TH/f.py
+++ TH/f.py
@@ -68,3 +68,60 @@ m = p.match('CrowHello')
 
 m = re.match('Crow|Serve', 'CrowHello')
 print(m[0])
+
+
+## softmax 
+
+import tensorflow as tf
+
+X = tf.placeholder(tf.float32, [None, 128])
+Y = tf.placeholder(tf.int32, [None, 1])  
+
+Y_one_hot = tf.one_hot(Y, 4)  # 감정 4가지
+Y_one_hot = tf.reshape(Y_one_hot, [-1, 4]) 
+
+W1 = tf.Variable(tf.random_normal([128, 4]), name='weight1')
+b1 = tf.Variable(tf.random_normal([4]), name='bias1')
+layer1 = tf.nn.relu(tf.matmul(X,W1)+b1)
+
+W2 = tf.Variable(tf.random_normal([4, 4]), name='weight2')
+b2 = tf.Variable(tf.random_normal([4]), name='bias2')
+layer2 = tf.nn.tanh(tf.matmul(layer1,W2)+b2)
+
+W3 = tf.Variable(tf.random_normal([4, 4]), name='weight2')
+b3 = tf.Variable(tf.random_normal([4]), name='bias2')
+layer3 = tf.matmul(layer2,W3)+b3
+
+#logits = tf.matmul(X, W) + b
+hypothesis = tf.nn.softmax(layer3)
+
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=layer3,labels=Y_one_hot))
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01).minimize(cost)
+
+prediction = tf.argmax(hypothesis, 1)
+correct_prediction = tf.equal(prediction, tf.argmax(Y_one_hot, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+sess = tf.Session() # session open
+sess.run(tf.global_variables_initializer())
+
+for step in range(7001):
+    sess.run(optimizer, feed_dict={X: x_train, Y: y_train})
+    if step % 1000 == 0:
+        loss, acc = sess.run([cost, accuracy], feed_dict={ X: x_train, Y: y_train})
+        print("Step: {:5}\tLoss: {:.3f}\tAcc: {:.2%}".format( step, loss, acc))
+
+
+pred = sess.run(prediction, feed_dict={X: x_test})
+acc = sess.run(accuracy, feed_dict={X: x_test, Y: y_test})
+print(acc)
+
+# 17:43 마지막 은닉층에서 항등으로 내보내니 결과 좋아짐
+
+sess.close() # session close
+
+import sys
+sys.path.append('/Users/hbk/github/it_project2/TH/')
+
+import mlpcPre
+mlpcPre.main('/Users/hbk/data/python-docs-samples/speech/cloud-client/sampling_cut/8_3_N.wav')
---
+++ TH/main.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import sys
+import os
+import librosa
+import numpy as np
+from sklearn.neural_network import MLPClassifier  # 다중분류에 해당되는 패키지 사용
+
+# 음성파일 경로
+path = '/Users/hbk/data/python-docs-samples/speech/cloud-client/sampling_cut/' 
+
+
+# 감정분석할 음성파일
+sound_path = '/Users/hbk/data/python-docs-samples/speech/cloud-client/sampling_cut/10_6_N.wav'  
+
+
+## 음성파일 melspectrogram으로 분석된 값으로 리턴
+
+def arrayStack(path): # input : 경로값 (ex. 'Users/.../')
+    files = os.listdir(path) # 해당경로 폴더에 담긴 wav 파일목록 저장
+    x = np.arange(128)
+    y = np.arange(1)
+    
+    for i in files:
+        label = i.split('.')[0].split('_')[-1] # 파일명에서 감정부분 추출 -> 숫자로 임의부여
+                
+        if label == 'S': # 슬픔
+            l = 0
+        elif label == 'N': # 중립
+            l = 1
+        elif label == 'J': # 즐거움
+            l = 2
+        elif label == 'A': # 화남
+            l = 3   
+  
+        r,sr = librosa.load(path+i, mono=True, sr=16000) # librosa 사용(모노, 샘플레이트 : 16000)
+        mel = librosa.feature.melspectrogram(y=r, sr=sr) # 음성에는 melspectrogram이 좋다고 해서 
+        a = np.mean(mel,axis=1) # 열 중간값 구함
+        
+        x = np.vstack([x,a]) # 적재
+        y = np.vstack([y,np.array(l)]) # 적재
+        
+    return x[1:], y[1:]
+
+
+## 훈련 및 테스트 데이터셋 세팅
+    
+def dataSet(x,y): # x : 데이터값, y : 라벨값
+    n = len(x)
+    import random
+    train_index = random.sample(range(n),int(n*0.8))
+    test_index = [i for i in range(n) if i not in train_index]
+    
+    from sklearn.preprocessing import normalize
+    # 훈련데이터는 전체데이터 80% 랜덤 추출
+    x_train = normalize(x[train_index]) 
+    y_train = y[train_index]
+    # 테스트데이터는 전체데이터 20% 랜덤 추출
+    x_test = normalize(x[test_index])
+    y_test = y[test_index]
+    
+    return x_train, y_train, x_test, y_test 
+
+
+## 데이터 분석(MLPClassifier)
+
+def mlpc(): # 훈련 및 검증결과 출력이후 모델을 리턴
+    
+    global x_train, y_train, x_test, y_test 
+    
+    # 훈련을 통해 모델을 생성
+    mlp_multilabel = MLPClassifier(hidden_layer_sizes=(128,128), max_iter=1000, random_state=None).fit(x_train, y_train)
+    acc1 = mlp_multilabel.score(x_train, y_train) # 정확도
+    print('\n모델 정확도 :',acc1)
+
+    # 모델 검증
+    y_pred = mlp_multilabel.predict(x_test)
+    print('모델 검증 :',np.sum(np.sum(y_test.astype(int) & y_pred, axis=1) > 0)/y_test.shape[0])
+    
+    return mlp_multilabel # 모델
+
+
+## 감정 예측
+    
+def dataMake(sound_path): # input : 음성파일이 담겨있는 파일경로 ex. "/Users/.../sori.wav"
+    # 음성데이터 분석    
+    r,sr = librosa.load(sound_path, mono=True, sr=16000) # librosa 사용(모노, 샘플레이트 : 16000)
+    mel = librosa.feature.melspectrogram(y=r, sr=sr)
+    #mfcc = librosa.feature.mfcc(r,sr)
+    a = np.mean(mel,axis=1) # 열 평균값 구함
+    
+    from sklearn.preprocessing import normalize
+    data = normalize(a.reshape(1,128))
+    
+    return data
+
+  
+def pred(model,sound_path):
+    pred1 = model.predict(dataMake(sound_path))  # mlpc()에서 생성된 모델에 의한 예측값
+
+    for i in pred1:
+        if i == 0:
+            res = "슬픔"
+        elif i == 1:
+            res = "중립"
+        elif i == 2:
+            res = "기쁨"
+        elif i == 3:
+            res = "화남"
+    return res
+
+
+## 메인함수
+    
+def main(): 
+    global path, sound_path
+    x,y = arrayStack(path)
+    x_train, y_train, x_test, y_test = dataSet(x,y)
+    model = mlpc() # 모델생성
+    return pred(model, sound_path)
+ 
+    
+
+if __name__ == '__main__':
+    
+    print('감정결과 :',main())    
+
+ 
