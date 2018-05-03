6a1fcd1e7c5985dcc4123d51a446efbf9f11c63a
INGYEOMENTAL <37355339+TAEHYOKIM@users.noreply.github.com>
2018-05-03 13:56:03 +0900

    수정

--- TH/data_down.py
+++ TH/data_down.py
@@ -14,6 +14,6 @@ y_data.to_csv("/Users/hbk/github/it_project2/TH/y_data.csv",mode = "w")
 ### 다운 ###
 import pandas as pd
 
-x_var = pd.read_csv("/Users/hbk/github/it_project2/TH/x_data.csv", index_col = 0)
-y_var = pd.read_csv("/Users/hbk/github/it_project2/TH/y_data.csv", index_col = 0)
+x_var = pd.read_csv("/Users/hbk/github/it_project2/TH/x_data.csv", index_col = 0).values
+y_var = pd.read_csv("/Users/hbk/github/it_project2/TH/y_data.csv", index_col = 0).values
 
